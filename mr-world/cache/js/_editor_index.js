3a700224e8f037318a08d1f44a57bfe82daf9f86
MikeMartemianov <mike.martemianov@gmail.com>
2025-01-19 07:23:23 +0200

    xffb
    zd

---
+++ mr-world/cache/js/_editor_index.js
@@ -0,0 +1,4 @@
+import * as __wonderlandengine_components from '@wonderlandengine/components';
+_registerEditor(__wonderlandengine_components);
+import * as _C__Users_mikem_Documents_WonderlandEngine_mr_world_js_button_js from 'C:/Users/mikem/Documents/WonderlandEngine/mr-world/js/button.js';
+_registerEditor(_C__Users_mikem_Documents_WonderlandEngine_mr_world_js_button_js);
